bf20bf3976fc3647bbd9ecde5ec72de4ee85d253
StevenDev <bossiolealbl@gmail.com>
2024-04-26 18:01:15 -0500

    estilo inicial login screen

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { RegisterScreen } from './RegisterScreen'
 
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className='auth_title'>Login</h3>
       <form>
         <input 
           type="email" 
           placeholder='email'
           name='email'
+          className='auth_input'
+          autoComplete='off'
         />
         <input 
           type="password" 
           placeholder="password"
           name="password" 
+          className='auth_input'
+          autoComplete='off'
           />
           <button
             type="submit"  
+            className='auth_button'
           >
             Login
           </button>
           <hr />
-          login
+          <div> <p> Login with social network</p> </div>
+          <Link to="/auth/register">You don't have an account?</Link>
       </form>
     </>
   )
